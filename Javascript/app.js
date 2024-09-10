48024c3f5fb0c7b2448591a9df43d5cd83dae03b
caritohernandez <carolinakasumi@gmail.com>
2024-09-09 21:11:24 -0500

    clase javascript

---
+++ Javascript/app.js
@@ -0,0 +1,10 @@
+console.log("hola mundo");
+//variables
+let numero1 =45
+let numero2 =39
+console.log(numero1);
+console.log(numero2);
+
+//concatenar valores
+console.log('LA SUMA DE',numero1,'y',numero2,'es:',suma);
+
